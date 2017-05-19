138a17fbfafa2c061829ef984b78596b3e713e46
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-05-19 15:06:02 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -2,44 +2,74 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import { get } from 'axios';
 import './App.css';
-const data = require('../Quotes.json')
+import $ from 'jquery';
+// import jsonp from 'jsonp';
+// import fetchJsonp from 'fetch-jsonp';
+// require('promise/polyfill-done');
+// const data = require('../Quotes.json')
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quoteData: [],
+      // quoteData: [],
       quote: "",
       name: "",
-      selectedQuote: null,
+      // selectedQuote: null,
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.getQuote = this.getQuote.bind(this);
+    this.tweet = this.tweet.bind(this);
 
   }
 
-  componentDidMount() {
-    // get(`../quotes.json`)
-    //   .then(({ data: quoteData }) => {
-    //     console.log(quoteData.split("\n"));
-    //     this.setState({ quoteData });
-    //     console.log(typeof quoteData);
-    //   });
-    this.setState({
-      quoteData: data,
+  // componentDidMount() {
+  // this.setState({
+  //   quoteData: data,
+  // });
+  // }
+
+  // handleClick(e) {
+  //   const quoteIndex = Math.floor(Math.random() * 101);
+  //   const { quoteData } = this.state;
+  //   this.setState({
+  //     quote: quoteData[quoteIndex].quote,
+  //     name: quoteData[quoteIndex].name,
+  //     selectedQuote: quoteIndex,
+  //   })
+
+  // }
+
+  getQuote() {
+    // fetchJsonp("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?"), null, function (err, data) {
+    //   if (err) {
+    //     console.error(err.message);
+    //   } else {
+    //     console.log(data);
+    //   }
+    // };
+    const _this = this;
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function (data) {
+      // console.log(data);
+      _this.setState({
+        quote: data.quoteText,
+        name: data.quoteAuthor,
+      });
     });
+    // jsonp('https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?', null, function (err, data) {
+    //   if (err) {
+    //     console.error(err.message);
+    //   } else {
+    //     console.log(data);
+    //   }
+    // });
   }
 
-  handleClick(e) {
-    const quoteIndex = Math.floor(Math.random() * 101);
-
-    this.setState({
-      quote: data[quoteIndex].quote,
-      name: data[quoteIndex].name,
-      selectedQuote: quoteIndex,
-    })
-
+  tweet() {
+    const { quote, name } = this.state;
+    const textToTweet = quote + ' --' + name;
+    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(textToTweet), '_blank')
   }
 
   render() {
@@ -47,7 +77,8 @@ class App extends Component {
     return (
       <div className="App">
         <Quote quote={quote} name={name} />
-        <button className="btn" onClick={this.handleClick}>Get quote</button>
+        <button className="btn" onClick={this.getQuote}>Get quote</button>
+        <button className="btn" onClick={this.tweet}>Tweet</button>
       </div>
 
     );
@@ -56,8 +87,9 @@ class App extends Component {
 
 const Quote = ({ quote, name }) => (
   <div className="quote">
-    {quote}
-    {name}
+    <p>{quote}</p>
+    <p>{name}</p>
+
   </div>
 )
 
